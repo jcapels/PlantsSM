31552187b08954b57d01ca57d4bad289226b2dc0
João Capela <pg38274@alunos.uminho.pt>
2022-10-20 15:55:25 +0100

    [ADD] prototype of model

---
+++ src/plants_sm/models/model.py
@@ -0,0 +1,33 @@
+from abc import ABCMeta, abstractmethod
+
+from pydantic import BaseModel
+
+from plants_sm.data_structures.dataset import Dataset
+
+
+class Model(metaclass=ABCMeta, BaseModel):
+
+    @abstractmethod
+    def _preprocess_data(self, dataset: Dataset):
+        pass
+
+    @abstractmethod
+    def _fit_data(self, dataset: Dataset, validation_dataset: Dataset):
+        pass
+
+    @abstractmethod
+    def _predict_proba(self, dataset: Dataset):
+        pass
+
+    def fit(self, dataset: Dataset, validation_dataset: Dataset):
+
+        return self._fit_data(dataset, validation_dataset)
+
+    def predict_proba(self, dataset: Dataset):
+
+        return self._predict_proba(dataset)
+
+    def preprocess(self, dataset: Dataset):
+
+        return self._preprocess_data(dataset)
+
