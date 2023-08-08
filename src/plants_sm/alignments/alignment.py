bcbb973b1edd9953c64c05367a6a4b96852a92c3
jcapels <jcapels96@gmail.com>
2023-08-08 11:12:39 +0100

    [FIX] alignment

--- src/plants_sm/alignments/alignment.py
+++ src/plants_sm/alignments/alignment.py
@@ -6,6 +6,10 @@ import pandas as pd
 
 class Alignment(ABC):
 
+    def __init__(self, database):
+        self.results = None
+        self.database = database
+
     @abstractmethod
     def create_database(self, fasta_file, db_name):
         pass
@@ -24,31 +28,56 @@ class Alignment(ABC):
     def _run(self, query_file, output_file, evalue, num_hits, output_options_str):
         pass
 
+    def associate_to_ec(self, database_ec_file, output_file):
+        database = pd.read_csv(database_ec_file)
+
+        new_results = pd.DataFrame(columns=database.columns)
+        for i, row in self.results.iterrows():
+            accession = row["qseqid"]
+            database_accession = row["qseqid"]
+
+            hit_row = database[database.loc[:, "accession"] == database_accession]
+
+            if not hit_row.empty:
+                new_results = pd.concat((new_results, hit_row))
+                new_results.at[i, "accession"] = accession
+                new_results.at[i, "name"] = accession
+                new_results.at[i, "sequence"] = pd.NA
+
+            else:
+                new_results.at[i, :] = pd.NA
+                new_results.at[i, "accession"] = accession
+                new_results.at[i, "name"] = accession
+                new_results.at[i, "sequence"] = pd.NA
+                new_results.at[i, :].fillna(0, inplace=True)
+
+        new_results.to_csv(output_file, index=False)
+
 
 class Diamond(Alignment):
 
     def __init__(self, database):
-        self.database = database
+        super().__init__(database)
 
     def create_database(self, fasta_file, db_name):
         subprocess.run(["diamond", "makedb", "--in", fasta_file, "--db", db_name])
 
     def _run(self, query_file, output_file, evalue, num_hits, output_options_str):
-
-        subprocess.run(["diamond", "blastp", "-d", self.database, "-q", query_file, "-o", output_file,
-                            "-f", f"6 {output_options_str}", "-evalue", str(evalue), "-max_target_seqs", str(num_hits)])
+        subprocess.call(f"diamond blastp -d {self.database} -q {query_file} -o {output_file} --outfmt 6 "
+                       f"{output_options_str} --evalue {evalue} --max-target-seqs {num_hits}", shell=True)
 
 
 class BLAST(Alignment):
 
     def __init__(self, database):
-        self.database = database
-        self.results = None
+        super().__init__(database)
 
     def create_database(self, fasta_file, db_name):
         subprocess.run(["makeblastdb", "-in", fasta_file, "-dbtype", "prot", "-out", db_name])
 
     def _run(self, query_file, output_file, evalue, num_hits, output_options_str):
-
         subprocess.run(["blastp", "-query", query_file, "-db", self.database, "-out", output_file, "-outfmt",
-                    f"6 {output_options_str}", "-evalue", str(evalue), "-max_target_seqs", str(num_hits)])
+                        f"6 {output_options_str}", "-evalue", str(evalue), "-max_target_seqs", str(num_hits)])
+
+
+
--- tests/unit_tests/alignments/test_alignments.py
+++ tests/unit_tests/alignments/test_alignments.py
@@ -2,11 +2,11 @@ import os
 import unittest
 from unittest import skip
 
-from plants_sm.alignments.alignment import BLAST
+from plants_sm.alignments.alignment import BLAST, Diamond
 from tests import TEST_DIR
 
 
-@skip("BLAST and diamond not being installed in CI")
+#@skip("BLAST and diamond not being installed in CI")
 class TestAlignments(unittest.TestCase):
 
     def test_blast_create_database(self):
@@ -25,9 +25,29 @@ class TestAlignments(unittest.TestCase):
 
     def test_diamond_run_alignment(self):
         DIAMOND_DB = "data/DIAMOND_DB"
-        blast = BLAST(DIAMOND_DB)
+        blast = Diamond(DIAMOND_DB)
         blast.create_database(os.path.join(TEST_DIR, "data", "test.fasta"), DIAMOND_DB)
         blast.run(os.path.join(TEST_DIR, "data", "query.fasta"),
-                  os.path.join(TEST_DIR, "data", "out.tsv"), evalue=1e-5, num_hits=2)
+                  os.path.join(TEST_DIR, "data", "out.csv"), evalue=1e-5, num_hits=2)
 
         self.assertTrue(blast.results.shape[0] == 6)
+
+    def test_diamond_associate_to_ec(self):
+        DIAMOND_DB = "data/DIAMOND_DB"
+        blast = Diamond(DIAMOND_DB)
+        blast.create_database(os.path.join(TEST_DIR, "data", "test.fasta"), DIAMOND_DB)
+        blast.run(os.path.join(TEST_DIR, "data", "query.fasta"),
+                  os.path.join(TEST_DIR, "data", "out.csv"), evalue=1e-5, num_hits=1)
+
+        blast.associate_to_ec(os.path.join(TEST_DIR, "data", "test_database_ec_numbers.csv"),
+                              os.path.join(TEST_DIR, "data", "out_ec.csv"))
+
+    def test_blast_associate_to_ec(self):
+        blast_DB = "data/blast_db"
+        blast = BLAST(blast_DB)
+        blast.create_database(os.path.join(TEST_DIR, "data", "test.fasta"), blast_DB)
+        blast.run(os.path.join(TEST_DIR, "data", "query.fasta"),
+                  os.path.join(TEST_DIR, "data", "out.csv"), evalue=1e-5, num_hits=1)
+
+        blast.associate_to_ec(os.path.join(TEST_DIR, "data", "test_database_ec_numbers.csv"),
+                              os.path.join(TEST_DIR, "data", "out_ec.csv"))
