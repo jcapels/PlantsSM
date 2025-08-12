87f94734ade0ca8c94b6346129cfa4cb08211033
jcapels <jcapels96@gmail.com>
2025-08-12 17:45:12 +0100

    [ADD] test

--- tests/unit_tests/pathway_prediction/test_ec_numbers_annotator.py
+++ tests/unit_tests/pathway_prediction/test_ec_numbers_annotator.py
@@ -4,11 +4,11 @@ from unittest import TestCase, skip
 import pandas as pd
 
 from plants_sm.pathway_prediction.ec_numbers_annotator import ProtBertECAnnotator
-from plants_sm.pathway_prediction.ec_numbers_annotator_utils.predictions import predict_with_model, predict_with_model_from_fasta
 
+from plants_sm.pathway_prediction.ec_numbers_annotator_utils.prot_bert_prediction import predict_from_csv, predict_with_model_from_fasta
 from tests import TEST_DIR
 
-# @skip("Require model download")
+@skip("Require model download")
 class TestEcNumbersAnnotatorUtils(TestCase):
 
     def setUp(self):
@@ -16,7 +16,7 @@ class TestEcNumbersAnnotatorUtils(TestCase):
         self.fasta_data_path = os.path.join(TEST_DIR, "data", "test.fasta")
 
     def test_make_prediction_protbert(self):
-        results = predict_with_model(pipeline="DNN ProtBERT all data",
+        results = predict_from_csv(
                            dataset_path=self.data_path,
                            output_path="predictions_protbert.csv",
                            ids_field="id",
@@ -26,7 +26,7 @@ class TestEcNumbersAnnotatorUtils(TestCase):
         self.assertEqual(type(results), pd.DataFrame)
 
     def test_make_prediction_protbert_from_fasta(self):
-        results = predict_with_model_from_fasta(pipeline="DNN ProtBERT all data",
+        results = predict_with_model_from_fasta(
                            fasta_path=self.fasta_data_path,
                            output_path="predictions_protbert_fasta.csv",
                            device="cpu")
@@ -44,20 +44,33 @@ class TestEcNumbersAnnotator(TestCase):
         # Assuming the annotator has a method to annotate from fasta
         solution = self.annotator.annotate_from_fasta(self.fasta_data_path)
         self.assertIsNotNone(solution)
-        self.assertTrue(hasattr(solution, 'enzymes_ec_1'))
-        self.assertTrue(hasattr(solution, 'enzymes_ec_2'))
-        self.assertTrue(hasattr(solution, 'enzymes_ec_3'))
-        self.assertTrue(hasattr(solution, 'enzymes_ec_4'))
+        self.assertTrue(hasattr(solution, 'entity_ec_1'))
+        self.assertTrue(hasattr(solution, 'entity_ec_2'))
+        self.assertTrue(hasattr(solution, 'entity_ec_3'))
+        self.assertTrue(hasattr(solution, 'entity_ec_4'))
 
-        self.assertTrue(hasattr(solution, 'enzymes'))
+        self.assertTrue(hasattr(solution, 'entities'))
+
+        solution.get_score("P12345", "EC1")
+
+    def test_annotate_from_fasta_wgpu(self):
+        # Assuming the annotator has a method to annotate from fasta
+        solution = self.annotator.annotate_from_fasta(self.fasta_data_path, device="cuda:0")
+        self.assertIsNotNone(solution)
+        self.assertTrue(hasattr(solution, 'entity_ec_1'))
+        self.assertTrue(hasattr(solution, 'entity_ec_2'))
+        self.assertTrue(hasattr(solution, 'entity_ec_3'))
+        self.assertTrue(hasattr(solution, 'entity_ec_4'))
+
+        self.assertTrue(hasattr(solution, 'entities'))
 
         solution.get_score("P12345", "EC1")
 
     def test_annotate_from_csv(self):
         solution = self.annotator.annotate_from_csv(self.data_path)
         self.assertIsNotNone(solution)
-        self.assertTrue(hasattr(solution, 'enzymes_ec_1'))
-        self.assertTrue(hasattr(solution, 'enzymes_ec_2'))
-        self.assertTrue(hasattr(solution, 'enzymes_ec_3'))
-        self.assertTrue(hasattr(solution, 'enzymes_ec_4'))
-        self.assertTrue(hasattr(solution, 'enzymes'))
+        self.assertTrue(hasattr(solution, 'entity_ec_1'))
+        self.assertTrue(hasattr(solution, 'entity_ec_2'))
+        self.assertTrue(hasattr(solution, 'entity_ec_3'))
+        self.assertTrue(hasattr(solution, 'entity_ec_4'))
+        self.assertTrue(hasattr(solution, 'entities'))
